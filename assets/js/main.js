fa17c053122ea577214f60941e076a4cd3482a4e
mhsohal <mhbuetdu@gmail.com>
2022-04-14 14:00:54 +0600

    faq 01 widget

--- assets/js/main.js
+++ assets/js/main.js
@@ -146,22 +146,7 @@ $('.plan-tab-switcher').on('click', function () {
   $('.plan-area').toggleClass('change-subs-duration');
 });
 
-// faq
-$('.faq-wrapper .faq-title').on('click', function (e) {
-  var element = $(this).parent('.faq-item');
-  if (element.hasClass('open')) {
-    element.removeClass('open');
-    element.find('.faq-content').removeClass('open');
-    element.find('.faq-content').slideUp(300, "swing");
-  } else {
-    element.addClass('open');
-    element.children('.faq-content').slideDown(300, "swing");
-    element.siblings('.faq-item').children('.faq-content').slideUp(300, "swing");
-    element.siblings('.faq-item').removeClass('open');
-    element.siblings('.faq-item').find('.faq-title').removeClass('open');
-    element.siblings('.taq-item').find('.faq-content').slideUp(300, "swing");
-  }
-});
+
 
 // slider
 // var swiper = new Swiper('.brand-slider', {
