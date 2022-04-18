b404ddd37cd79d65a24f85ccd31fb5dbfa093877
Md Rakibul Islam <irakibul189@gmail.com>
2022-04-18 15:21:34 +0600

    service content

---
+++ template-parts/content-service.php
@@ -0,0 +1,16 @@
+<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 mb-30">
+    <div class="service-item three">
+        <?php if (has_post_thumbnail()){?>
+            <div class="service-icon">
+                <?php the_post_thumbnail('full');?>
+            </div>
+        <?php } ?>
+        <div class="service-content">
+            <h3 class="title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
+            <p><?php get_the_excerpt();?></p>
+            <div class="service-btn">
+                <a href="<?php the_permalink();?>" class="custom-btn"><?php echo esc_html('Learn More')?> <i class="icon-Group-2361 ml-2"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
