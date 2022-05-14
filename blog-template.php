674f7546814b4851f12376585b9fcdad5468f6d6
Md Rakibul Islam <irakibul189@gmail.com>
2022-05-14 19:47:23 +0600

    blog and css fix

---
+++ blog-template.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * The Main Template File
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package softim
+ */
+
+get_header();
+?>
+    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        Start Blog
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+    <section class="blog-section ptb-120">
+        <div class="container">
+            <div class="row justify-content-center mb-60-none">
+                <div class="col-xl-8 col-lg-8 mb-60">
+                    <div class="row justify-content-center mb-60-none">
+                        <?php
+                        if (have_posts()) :
+
+                            /* Start the Loop */
+                            while (have_posts()) :
+                                the_post();
+                                /*
+                                 * Include the Post-Type-specific template for the content.
+                                 * If you want to override this in a child theme, then include a file
+                                 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+                                 */
+                                get_template_part('template-parts/content', 'layout-2');
+
+                            endwhile;
+
+                        else :
+
+                            get_template_part('template-parts/content', 'none');
+
+                        endif;
+                        ?>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 mb-60">
+                    <?php get_sidebar(); ?>
+                </div>
+            </div>
+            <nav>
+                <ul class="pagination">
+                    <li class="page-item prev">
+                        <a class="page-link" href="#" rel="prev" aria-label="Prev &raquo;">PREV</a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">01</a></li>
+                    <li class="page-item active" aria-current="page"><span class="page-link">02</span></li>
+                    <li class="page-item"><a class="page-link" href="#">03</a></li>
+                    <li class="page-item"><a class="page-link" href="#">04</a></li>
+                    <li class="page-item"><a class="page-link" href="#">05</a></li>
+                    <li class="page-item next">
+                        <a class="page-link" href="#" rel="next" aria-label="Next &raquo;">NEXT</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </section>
+    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        End Blog
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        Start Blog
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+    <section class="blog-section ptb-120">
+        <div class="container">
+            <div class="row justify-content-center mb-60-none">
+                <div class="col-xl-8 col-lg-8 mb-60">
+                    <div class="row justify-content-center mb-60-none">
+                        <?php
+                        if (have_posts()) :
+
+                            /* Start the Loop */
+                            while (have_posts()) :
+                                the_post();
+                                /*
+                                 * Include the Post-Type-specific template for the content.
+                                 * If you want to override this in a child theme, then include a file
+                                 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+                                 */
+                                get_template_part('template-parts/content', get_post_format());
+
+                            endwhile;
+
+                        else :
+
+                            get_template_part('template-parts/content', 'none');
+
+                        endif;
+                        ?>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 mb-60">
+                    <?php get_sidebar(); ?>
+                </div>
+            </div>
+            <nav>
+                <?php softim()->post_pagination(); ?>
+            </nav>
+        </div>
+    </section>
+    <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        End Blog
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+<?php
+get_footer();
--- template-parts/content-layout-2.php
+++ template-parts/content-layout-2.php
@@ -8,7 +8,7 @@
  */
 
 ?>
-<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 mb-60">
+<div class="col-xl-12 mb-60">
     <div class="blog-item">
         <?php if (has_post_thumbnail()) { ?>
             <div class="blog-thumb">
@@ -18,10 +18,13 @@
         <div class="blog-content">
             <div class="blog-post-meta">
                 <span class="user"><?php echo esc_html('By : '); ?><?php the_author(); ?></span>
-                <span class="date"><?php echo get_the_date('F j, Y'); ?></span>
+                <span class="category two"><?php echo get_the_date('F j, Y'); ?></span>
             </div>
             <h3 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            <p><?php the_excerpt(); ?></p>
+            <div class="blog-btn">
+                <a href="<?php the_permalink(); ?>" class="custom-btn"><?php echo esc_html('Read More'); ?> <i class="fas fa-arrow-right ml-2"></i></a>
+            </div>
         </div>
     </div>
 </div>
-
